deb1053a69615c29dbd9b87694a349f2624a0276
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-11-26 19:11:13 -0300

    feat: add user.service

---
+++ src/user/services/user.service.ts
@@ -0,0 +1,14 @@
+import { IUserEntity } from '../entityes/user.entity';
+import { UserDto } from './dto/userInput.dto';
+import { randomUUID } from 'node:crypto';
+
+
+export class UserService {
+  private users: IUserEntity[] = [];
+
+  async createUser(user: UserDto): Promise<IUserEntity> {
+    const userEntity = { ...user, id: randomUUID() };
+    this.users.push(userEntity);
+    return userEntity;
+  }
+}
