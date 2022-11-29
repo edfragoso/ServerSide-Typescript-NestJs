73a6ac5e047ea7c3f38a2e44a5826cb0f35bbb7c
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-11-29 13:37:43 -0300

    refactor: refactor name route

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap(): Promise<void> {
 
   const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('api/docs', app, document);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(PORT, () => console.log(`App bootstraped at :${PORT}`));
 }
