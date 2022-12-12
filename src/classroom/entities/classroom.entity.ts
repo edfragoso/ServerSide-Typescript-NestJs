f8c3fe9a84b77b09fd0c0f651252f37ea9f68ab2
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-12-12 19:44:04 -0300

    refactor: refactor params entity

--- src/classroom/entities/classroom.entity.ts
+++ src/classroom/entities/classroom.entity.ts
@@ -1,5 +1,10 @@
+import { AttendanceList } from "../../attendance-list/entities/attendance-list.entity";
+import { IUserEntity } from "../../user/entityes/user.entity";
 import { CreateClassroomDto } from "../dto/create-classroom.dto";
 
 export class Classroom extends CreateClassroomDto {
     id: string;
+    students: IUserEntity[];
+    teachers: IUserEntity[];
+    attendances: AttendanceList[];
 }
