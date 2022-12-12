83b3e1c397cf80b74e23cb7855d2c0e1aca5a6f6
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-12-12 16:32:46 -0300

    feat: create class attendanceList

--- src/attendance-list/entities/attendance-list.entity.ts
+++ src/attendance-list/entities/attendance-list.entity.ts
@@ -1,5 +1,10 @@
+import { IUserEntity } from "../../user/entityes/user.entity";
 import { CreateAttendanceListDto } from "../dto/create-attendance-list.dto";
 
 export class AttendanceList extends CreateAttendanceListDto {
     id: string;
+    startDate: Date;
+    endDate: Date;
+    students: IUserEntity[] = [];
+    day: string;
 }
