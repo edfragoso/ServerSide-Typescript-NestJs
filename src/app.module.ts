30e33dd6fcf139785e2ee200d24723c68bd41f9d
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-12-12 10:55:12 -0300

    feat: create nest base structure to attendanceList

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { DatabaseModule } from './prisma/database.module';
 import { UserService } from './user/services/user.service';
 import { UserController } from './user/user.controller';
 import { UserRepository } from './user/user.repository';
+import { ClassModule } from './classroom/class/class.module';
+import { ClassModule } from './attendance-list/class/class.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, ClassModule],
   controllers: [UserController],
   providers: [UserService, UserRepository],
 })
