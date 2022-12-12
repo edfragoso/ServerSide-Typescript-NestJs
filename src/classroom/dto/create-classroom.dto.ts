468f311c64d82dc0068c1118761555ec5ea5f2e4
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-12-12 12:13:58 -0300

    feat: implement classroom entity

--- src/classroom/dto/create-classroom.dto.ts
+++ src/classroom/dto/create-classroom.dto.ts
@@ -1,1 +1,11 @@
-export class CreateClassroomDto {}
+import { AttendanceList } from "../../attendance-list/entities/attendance-list.entity";
+import { IUserEntity } from "../../user/entityes/user.entity";
+
+export class CreateClassroomDto {
+    name: string;
+    theme: string;
+    subject: string;
+    students: IUserEntity[];
+    teachers: IUserEntity[];
+    attendances: AttendanceList[]; 
+}
