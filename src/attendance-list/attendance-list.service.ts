44b5cbe92d01edd8612a2853a2a6359bc887b5e1
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-12-12 16:30:36 -0300

    feat: create attendanceList

--- src/attendance-list/attendance-list.service.ts
+++ src/attendance-list/attendance-list.service.ts
@@ -1,26 +1,52 @@
 import { Injectable } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { ClassroomService } from '../classroom/classroom.service';
 import { CreateAttendanceListDto } from './dto/create-attendance-list.dto';
 import { UpdateAttendanceListDto } from './dto/update-attendance-list.dto';
+import { AttendanceList } from './entities/attendance-list.entity';
 
 @Injectable()
 export class AttendanceListService {
-  create(createAttendanceListDto: CreateAttendanceListDto) {
-    return 'This action adds a new attendanceList';
+  private _attendanceList: AttendanceList[] = [];
+  constructor(private readonly classroomService: ClassroomService) {}
+
+  async create(
+    createAttendanceListDto: CreateAttendanceListDto,
+  ): Promise<AttendanceList> {
+    await this.classroomService.findOne(createAttendanceListDto.classroomId);
+    
+    const today = new Date(Date.now()).toISOString().slice(0,10);
+    const formatedToday = today.slice(8, 10) + 
+    "/" + today.slice(5, 7) + 
+    "/" + today.slice(0, 4);
+    const endDateToAttendance = 2 * 60 * 1000;
+    const attendanceToday = new AttendanceList();
+    
+    (attendanceToday.id = randomUUID()),
+      (attendanceToday.startDate = new Date(Date.now())),
+      (attendanceToday.endDate = new Date(Date.now() + endDateToAttendance)),
+      (attendanceToday.students = []),
+      //(attendanceToday.day = today),
+      (attendanceToday.day = formatedToday),
+      
+      this._attendanceList.push(attendanceToday);
+    
+      return Promise.resolve(attendanceToday);
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all attendanceList`;
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} attendanceList`;
   }
 
-  update(id: number, updateAttendanceListDto: UpdateAttendanceListDto) {
+  async update(id: number, updateAttendanceListDto: UpdateAttendanceListDto) {
     return `This action updates a #${id} attendanceList`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} attendanceList`;
   }
 }
