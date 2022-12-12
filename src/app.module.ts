0a95abd0128dbd523a8d04905c17025312c573aa
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-12-12 11:33:51 -0300

    feat: add classroomModule and attendanceListModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { UserController } from './user/user.controller';
 import { UserRepository } from './user/user.repository';
 import { ClassModule } from './classroom/class/class.module';
 import { ClassModule } from './attendance-list/class/class.module';
+import { ClassroomModule } from './classroom/classroom.module';
+import { AttendanceListModule } from './attendance-list/attendance-list.module';
 
 @Module({
-  imports: [DatabaseModule, ClassModule],
+  imports: [DatabaseModule, ClassModule, ClassroomModule, AttendanceListModule],
   controllers: [UserController],
   providers: [UserService, UserRepository],
 })
