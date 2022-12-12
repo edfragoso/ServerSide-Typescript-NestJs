6f7c2057eb7ee2e0fa666ab43939f9eb8d71ca58
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-12-12 12:13:31 -0300

    feat: implement attendentList

--- src/attendance-list/dto/create-attendance-list.dto.ts
+++ src/attendance-list/dto/create-attendance-list.dto.ts
@@ -1,1 +1,9 @@
-export class CreateAttendanceListDto {}
+import { IUserEntity } from "../../user/entityes/user.entity";
+
+export class CreateAttendanceListDto {
+    classroomId: string;
+    stundents: IUserEntity[];
+    startDate: Date;
+    endDate: Date;
+    todayDate: Date;
+}
