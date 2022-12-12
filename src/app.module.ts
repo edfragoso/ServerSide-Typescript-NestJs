ff890a302f1f4a14603a86fa7e8576a30128dd17
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-12-12 16:33:59 -0300

    feat: create classroomService

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,13 @@ import { DatabaseModule } from './prisma/database.module';
 import { UserService } from './user/services/user.service';
 import { UserController } from './user/user.controller';
 import { UserRepository } from './user/user.repository';
-import { ClassModule } from './classroom/class/class.module';
-import { ClassModule } from './attendance-list/class/class.module';
+
+
 import { ClassroomModule } from './classroom/classroom.module';
 import { AttendanceListModule } from './attendance-list/attendance-list.module';
 
 @Module({
-  imports: [DatabaseModule, ClassModule, ClassroomModule, AttendanceListModule],
+  imports: [DatabaseModule, ClassroomModule, AttendanceListModule],
   controllers: [UserController],
   providers: [UserService, UserRepository],
 })
