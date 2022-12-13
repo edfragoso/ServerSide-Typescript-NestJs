c41cd1ec101a5fef79e21c971633150a1df47d5e
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-12-13 18:28:52 -0300

    feat: add import prismaModule

--- src/attendance-list/attendance-list.module.ts
+++ src/attendance-list/attendance-list.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AttendanceListService } from './attendance-list.service';
 import { AttendanceListController } from './attendance-list.controller';
 import { ClassroomService } from '../classroom/classroom.service';
+import { AttendanceListRepository } from './attendance-list.repository';
+import { DatabaseModule } from '../prisma/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [AttendanceListController],
-  providers: [AttendanceListService, ClassroomService]
+  providers: [AttendanceListService, ClassroomService, AttendanceListRepository]
 })
 export class AttendanceListModule {}
