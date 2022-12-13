62466355f08f1b52db723d6fa28ca6a31030f838
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-12-13 17:47:43 -0300

    feat: implement attendanceListRepository

--- src/attendance-list/attendance-list.service.ts
+++ src/attendance-list/attendance-list.service.ts
@@ -3,24 +3,26 @@ import { randomUUID } from 'crypto';
 import { ClassroomService } from '../classroom/classroom.service';
 import { Exception } from '../utils/exceptions/exception';
 import { Exceptions } from '../utils/exceptions/exceptionsHelper';
+import { AttendanceListRepository } from './attendance-list.repository';
 import { CreateAttendanceListDto } from './dto/create-attendance-list.dto';
 import { UpdateAttendanceListDto } from './dto/update-attendance-list.dto';
 import { AttendanceList } from './entities/attendance-list.entity';
 
 @Injectable()
 export class AttendanceListService {
-  private _attendanceList: AttendanceList[] = [];
-  constructor(private readonly classroomService: ClassroomService) {}
+  constructor(
+    private readonly classroomService: ClassroomService,
+    private readonly attendanceLisRepository: AttendanceListRepository,
+  ) {}
 
   async create(
     createAttendanceListDto: CreateAttendanceListDto,
   ): Promise<AttendanceList> {
     await this.classroomService.findOne(createAttendanceListDto.classroomId);
-    
-    const today = new Date(Date.now()).toISOString().slice(0,10);
-    const formatedToday = today.slice(8, 10) + 
-    "/" + today.slice(5, 7) + 
-    "/" + today.slice(0, 4);
+
+    const today = new Date(Date.now()).toISOString().slice(0, 10);
+    const formatedToday =
+      today.slice(8, 10) + '/' + today.slice(5, 7) + '/' + today.slice(0, 4);
     const endDateToAttendance = 2 * 60 * 1000;
     const attendanceToday: AttendanceList = {
       ...createAttendanceListDto,
@@ -31,38 +33,41 @@ export class AttendanceListService {
       //day: today,
       day: formatedToday,
     };
-    
-      
-      this._attendanceList.push(attendanceToday);
-    
-      return Promise.resolve(attendanceToday);
+
+    return await this.attendanceLisRepository.createAttendanceList(
+      attendanceToday,
+    );
   }
 
   async findAll() {
-    return this._attendanceList;
+    return await this.attendanceLisRepository.allAttendancesLists();
   }
 
   async findOne(id: string): Promise<AttendanceList> {
-    const findAttendanceList = this._attendanceList.find((AttendanceList) => AttendanceList.id === id,
-    );
-    return findAttendanceList;
-
+    const findedById = await this.attendanceLisRepository.attendanceListById(id) 
+    return findedById;
   }
 
-  async update(id: string, updateAttendanceListDto: UpdateAttendanceListDto) {
-    return ;
+  async update(updateAttendanceListDto: UpdateAttendanceListDto, ) {
+    return await this.attendanceLisRepository.updateAttendanceList(
+      updateAttendanceListDto,
+    )
   }
 
-  async registerOnAttendanceList(attendanceListId: string, userId: string): Promise<string> {
+  async registerOnAttendanceList(
+    attendanceListId: string,
+    userId: string,
+  ): Promise<AttendanceList> {
     const findedAttendenceList = await this.findOne(attendanceListId);
-    const actualDate = new Date(Date.now())
-    if(actualDate.getTime() > findedAttendenceList.endDate.getTime()){
-      throw new Exception(Exceptions.InvalidData, "Ferrou")
+    const actualDate = new Date(Date.now());
+    if (actualDate.getTime() > findedAttendenceList.endDate.getTime()) {
+      throw new Exception(Exceptions.InvalidData, 'Ferrou');
     }
-    return "Efetuado com sucesso";
+
+    return await this.attendanceLisRepository.updateAttendanceList({
+      id: attendanceListId,
+      studentsId: [userId],
+    });
   }
 
-  async remove(id: number) {
-    return `This action removes a #${id} attendanceList`;
-  }
 }
