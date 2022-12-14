98ea18e81e529151c3941b4cb1eec08f981323a6
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-12-13 22:25:45 -0300

    feat: create classroomRepository

---
+++ src/classroom/classroom.repository.ts
@@ -0,0 +1,58 @@
+import { Classroom } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateClassroomDto } from './dto/create-classroom.dto';
+import { UpdateClassroomDto } from './dto/update-classroom.dto';
+
+export class ClassroomRepository {
+  private dataToReturn = {
+    students: true,
+    teachers: true,
+    attendances: {
+      include: {
+        students: true,
+      },
+    },
+  };
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createClassroom({
+    id,
+    name,
+    subject,
+    theme,
+  }: Classroom): Promise<Classroom> {
+    return await this.prismaService.classroom.create({
+      data: {
+        id: id,
+        name: name,
+        subject: subject,
+        theme: theme,
+      },
+      include: {
+        attendances: {
+          include: {
+            students: true,
+          },
+        },
+        students: true,
+        teachers: true,
+      },
+    });
+  }
+
+  async updateClassroom(updateData: UpdateClassroomDto): Promise<Classroom> {
+    return await this.prismaService.classroom.update({
+      where: { id: updateData.id },
+      data: {
+        ...updateData,
+        students: {
+          connect: updateData.studentsIds?.map((id) => ({ id: id })),
+        },
+        teachers: {
+          connect: updateData.teachersIds?.map((id) => ({ id: id })),
+        },
+      },
+      include: this.dataToReturn,
+    });
+  }
+}
