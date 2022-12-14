b3d41533915d13554410c79b6274405f75dc956e
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-12-14 00:08:19 -0300

    feat: add import databaseModule

--- src/classroom/classroom.module.ts
+++ src/classroom/classroom.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ClassroomService } from './classroom.service';
 import { ClassroomController } from './classroom.controller';
 import { ClassroomRepository } from './classroom.repository';
+import { DatabaseModule } from '../prisma/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [ClassroomController],
   providers: [ClassroomService, ClassroomRepository]
 })
