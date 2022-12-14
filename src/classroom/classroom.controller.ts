ee48da319d51304733900a7d2a23cdee9945ed66
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-12-14 16:23:15 -0300

    feat: add try {} catch {}

--- src/classroom/classroom.controller.ts
+++ src/classroom/classroom.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { HandleException } from '../utils/exceptions/exceptionsHelper';
 import { ClassroomService } from './classroom.service';
 import { CreateClassroomDto } from './dto/create-classroom.dto';
 import { UpdateClassroomDto } from './dto/update-classroom.dto';
@@ -9,26 +18,49 @@ export class ClassroomController {
 
   @Post()
   create(@Body() createClassroomDto: CreateClassroomDto) {
-    return this.classroomService.create(createClassroomDto);
+    try {
+      return this.classroomService.create(createClassroomDto);
+    } catch (err) {
+      HandleException(err);
+    }
   }
 
   @Get()
   findAll() {
-    return this.classroomService.findAll();
+    try {
+      return this.classroomService.findAll();
+    } catch (err) {
+      HandleException(err);
+    }
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.classroomService.findOne(id);
+    try {
+      return this.classroomService.findOne(id);
+    } catch (err) {
+      HandleException(err);
+    }
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateClassroomDto: UpdateClassroomDto) {
-    return this.classroomService.update(updateClassroomDto);
+  update(
+    @Param('id') id: string,
+    @Body() updateClassroomDto: UpdateClassroomDto,
+  ) {
+    try {
+      return this.classroomService.update(updateClassroomDto);
+    } catch (err) {
+      HandleException(err);
+    }
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.classroomService.remove(id);
+    try {
+      return this.classroomService.remove(id);
+    } catch (err) {
+      HandleException(err);
+    }
   }
 }
