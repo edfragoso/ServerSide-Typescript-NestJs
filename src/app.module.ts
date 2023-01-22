576328fb884b119874b5fc09f5f152b7b4b3545e
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-01-22 12:02:40 -0300

    feat: AuthModules @modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { DatabaseModule } from './prisma/database.module';
 import { AttendanceListModule } from './attendance-list/attendance-list.module';
 import { ClassroomModule } from './classroom/classroom.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, ClassroomModule, AttendanceListModule, UserModule],
-  
+  imports: [
+    DatabaseModule,
+    AuthModule,
+    UserModule,
+    ClassroomModule,
+    AttendanceListModule,
+  ],
 })
 export class AppModule {}
