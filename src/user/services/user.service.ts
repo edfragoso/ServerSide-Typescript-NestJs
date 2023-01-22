a9581a53c2838c829e43a033b94ffe8c4f1b2587
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-01-22 17:35:28 -0300

    refactor: add param role:'user'

--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -12,7 +12,7 @@ export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
 
   async createUser(user: UserDto): Promise<IUserEntity> {
-    const userEntity = { ...user, id: randomUUID() };
+    const userEntity = { ...user, id: randomUUID(), role: 'user' };
     if (user.password.length <= 7) {
       throw {
         message: 'Passoword less than 7 characters.',
